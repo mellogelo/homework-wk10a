803bd5d8e6c00f036ea51977ae1977ba4bef8286
Ralph Magbanua <rmagbanua09@gmail.com>
2020-05-29 00:44:06 -0700

    prompt

--- Develop/app.js
+++ Develop/app.js
@@ -13,7 +13,86 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function start() {
+    inquirer
+    .prompt([
+    {
+        type: "input",
+        message: "What is employee's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is employee's id?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is employee's email?",
+        name: "email"
+    },
+    {
+        type: "list",
+        message: "What is employee's title?",
+        name: "title",
+        choices: ["Engineer", "Intern", "Manager"]
+    }
+    ])
+    .then((data) => {
+        name = data.name;
+        id = data.id;
+        title = data.title;
+        email = data.email;
+    })
+};
 
+switch (title){
+    case "Manager":
+        await inquirer.prompt([
+            {
+                type: "input",
+                message: "What is your Manager's Office Number?",
+                name: "officeNumber"
+            }
+        ])
+        .then((data) => {
+            const manager = new Manager(name, id, email, data.officeNumber);
+
+            teamMember = fs.readFileSync("..Develop/templates/manager.html")
+        });
+        break;
+
+        case "Engineer":
+            await inquirer.prompt([
+                {
+                    type: "input",
+                    message: "What is your Engineer's GitHub?",
+                    name: "github"
+                }
+            ])
+            .then((data) => {
+                const engineer = new Engineer(name, id, email, data.github);
+    
+                teamMember = fs.readFileSync("..Develop/templates/engineer.html")
+            })
+            break;
+   
+        
+        case "Intern":
+            await inquirer.prompt([
+                {
+                    type: "input",
+                    message: "What is your school did your Intern go to?",
+                    name: "school"
+                }
+            ])
+            .then((data) => {
+                const intern = new Intern(name, id, email, data.school);
+    
+                teamMember = fs.readFileSync("..Develop/templates/intern.html")
+            })
+            break;
+}
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
